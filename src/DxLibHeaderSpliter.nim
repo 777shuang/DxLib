85f819979658fd586142a0af8dc6cb3cf50394fd
777shuang <shuang777@outlook.jp>
2023-07-03 22:25:53 +0900

    Update

--- src/DxLibHeaderSpliter.nim
+++ src/DxLibHeaderSpliter.nim
@@ -40,9 +40,11 @@ if not fileExists(zipFileName):
   except IOError:
     echo getCurrentExceptionMsg()
 
-var reader: ZipArchiveReader
+var lines: seq[string]
 try:
-  reader = openZipArchive(zipFileName)
+  var reader = openZipArchive(zipFileName)
+  lines = reader.extractFile("DxLibMake/DxLib.h").convert(srcEncoding="shiftjis", destEncoding="UTF-8").split("\r\n")
+  reader.close()
 except ZippyError:
   echo getCurrentExceptionMsg()
 
@@ -58,78 +60,89 @@ proc changeFlag(f: Flag) =
   flag = f
   fileWrite = open($f, FileMode.fmWrite)
 
-let lines = reader.extractFile("DxLibMake/DxLib.h").convert(srcEncoding="shiftjis", destEncoding="UTF-8").split("\r\n")
 # c2nim では入力ファイルが大きすぎるとエラーが出るため、内容を分割する
-for line in lines:
-  case flag
-  of NULL:
-    case line
-    of "#define DX_DEFINE_START":
-      changeFlag(DEFINE)
-      fileWrite.writeLine("#define SEEK_SET " & SEEK_SET().intToStr)
-      fileWrite.writeLine("#define SEEK_END " & SEEK_END().intToStr)
-      fileWrite.writeLine("#define SEEK_CUR " & SEEK_CUR().intToStr)
-    of "#define DX_STRUCT_START":
-      changeFlag(STRUCT)
-    of "#define DX_FUNCTION_START":
-      changeFlag(FUNCTION)
-      
-  of DEFINE:
-    if line == "#define DX_DEFINE_END":
-      flag = NULL
-      fileWrite.close()
-      const
-        before = splitFile($DEFINE).name & ".nim"
-        after = "src" / before
-      c2nim($DEFINE)
-      fileRead = open(before, FileMode.fmRead)
-      fileWrite = open(after, FileMode.fmWrite)
-      while not fileRead.endOfFile:
-        fileWrite.writeLine(fileRead.readLine.replace("div", "/").replace("(DX_FONTTYPE_NORMAL)", "0"))
-      fileRead.close()
-      fileWrite.close()
-    else:
-      fileWrite.writeLine(line)
+try:
+  for line in lines:
+    case flag
+    of NULL:
+      case line
+      of "#define DX_DEFINE_START":
+        changeFlag(DEFINE)
+        fileWrite.writeLine("#define SEEK_SET " & SEEK_SET().intToStr)
+        fileWrite.writeLine("#define SEEK_END " & SEEK_END().intToStr)
+        fileWrite.writeLine("#define SEEK_CUR " & SEEK_CUR().intToStr)
+      of "#define DX_STRUCT_START":
+        changeFlag(STRUCT)
+      of "#define DX_FUNCTION_START":
+        changeFlag(FUNCTION)
+        
+    of DEFINE:
+      # マクロ定義部
+      if line == "#define DX_DEFINE_END":
+        flag = NULL
+        fileWrite.close()
+        const
+          before = splitFile($DEFINE).name & ".nim"
+          after = "src" / before
+        c2nim($DEFINE)
+        fileRead = open(before, FileMode.fmRead)
+        fileWrite = open(after, FileMode.fmWrite)
+        while not fileRead.endOfFile:
+          fileWrite.writeLine(fileRead.readLine.replace("div", "/").replace("(DX_FONTTYPE_NORMAL)", "0"))
+        fileRead.close()
+        fileWrite.close()
+      else:
+        fileWrite.writeLine(line)
 
-  of STRUCT:
-    if line == "#define DX_STRUCT_END":
-      flag = NULL
-      fileWrite.close()
-      c2nim($STRUCT)
-      const
-        before = splitFile($STRUCT).name & ".nim"
-        after = "src" / before
-      fileRead = open(before, FileMode.fmRead)
-      fileWrite = open(after, FileMode.fmWrite)
-      fileWrite.writeLine("import winim")
-      fileWrite.writeLine("import DxDataType")
-      while not fileRead.endOfFile:
-        fileWrite.writeLine(fileRead.readLine)
-    else:
-      fileWrite.writeLine(line)
+    of STRUCT:
+      # 構造体定義部
+      if line == "#define DX_STRUCT_END":
+        flag = NULL
+        fileWrite.close()
+        c2nim($STRUCT)
+        const
+          before = splitFile($STRUCT).name & ".nim"
+          after = "src" / before
+        fileRead = open(before, FileMode.fmRead)
+        fileWrite = open(after, FileMode.fmWrite)
+        fileWrite.writeLine("import winim")
+        fileWrite.writeLine("import DxDefine")
+        while not fileRead.endOfFile:
+          fileWrite.writeLine(fileRead.readLine)
+        fileRead.close()
+        fileWrite.close()
+      else:
+        fileWrite.writeLine(line)
 
-  of  FUNCTION:
-    if line == "#define DX_FUNCTION_END":
-      flag = NULL
-      fileWrite.close()
-      c2nim("--cpp " & $FUNCTION)
-      const
-        before = splitFile($STRUCT).name & ".nim"
-        after = "src" / before
-      fileRead = open(before, FileMode.fmRead)
-      fileWrite = open(after, FileMode.fmWrite)
-      fileWrite.writeLine("import winim")
-      while not fileRead.endOfFile:
-        fileWrite.writeLine(fileRead.readLine)
-    else:
-      if not line.strip.startsWith("#"):
-        var buf:string = line
-        # DEFAULTPARAM を外す
-        while buf.contains("DEFAULTPARAM"):
-          buf = buf.replace(
-            re"(.*)(DEFAULTPARAM[  ]*\([  ]*=[  ]*)([^\)]*)([   ]*\))(.*)",
-            proc(match: RegexMatch): string = return $match.captures[0] & " = " & $match.captures[2] & $match.captures[4]
-          )
-        fileWrite.writeLine(buf.replace("__inline", "inline"))
-
-reader.close()
+    of  FUNCTION:
+      # 関数前方宣言部
+      if line == "#define DX_FUNCTION_END":
+        flag = NULL
+        fileWrite.close()
+        c2nim("--cpp " & $FUNCTION)
+        const
+          before = splitFile($FUNCTION).name & ".nim"
+          after = "src" / before
+        fileRead = open(before, FileMode.fmRead)
+        fileWrite = open(after, FileMode.fmWrite)
+        const ary = ["import winim", "import DxStruct", "import DxDll", "", "{.push header: DLL.}"]
+        for str in ary:
+          fileWrite.writeLine(str)
+        while not fileRead.endOfFile:
+          fileWrite.writeLine(fileRead.readLine)
+        fileWrite.writeLine("")
+        fileWrite.writeLine("{.pop.}")
+        fileRead.close()
+        fileWrite.close()
+      else:
+        if not line.strip.startsWith("#"):
+          var buf:string = line
+          # DEFAULTPARAM を外す
+          while buf.contains("DEFAULTPARAM"):
+            buf = buf.replace(
+              re"(.*)(DEFAULTPARAM[  ]*\([  ]*=[  ]*)([^\)]*)([   ]*\))(.*)",
+              proc(match: RegexMatch): string = return $match.captures[0] & " = " & $match.captures[2] & $match.captures[4]
+            )
+          fileWrite.writeLine(buf.replace("__inline", "inline"))
+except IOError:
+  echo getCurrentExceptionMsg()
